9acd69b74f5da2c23637693ee2a0b53e1d015e64
David Maes <david.s.maes@gmail.com>
2020-11-03 11:13:08 +0100

    1.16.4 support

--- StaffPlusPlusCraftbukkitAPI/src/main/java/be/garagepoort/staffplusplus/craftbukkit/api/ProtocolFactory.java
+++ StaffPlusPlusCraftbukkitAPI/src/main/java/be/garagepoort/staffplusplus/craftbukkit/api/ProtocolFactory.java
@@ -6,10 +6,7 @@ import net.shortninja.staffplus.server.compatibility.v1_13_R1.Protocol_v1_13_R1;
 import net.shortninja.staffplus.server.compatibility.v1_13_R2.Protocol_v1_13_R2;
 import net.shortninja.staffplus.server.compatibility.v1_14_R1.Protocol_v1_14_R1;
 import net.shortninja.staffplus.server.compatibility.v1_14_R2.Protocol_v1_14_R2;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_15_R1;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_16_R1;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_16_R2;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_16_R3;
+import net.shortninja.staffplus.server.compatibility.v1_1x.*;
 import org.bukkit.Bukkit;
 
 public class ProtocolFactory {
@@ -38,6 +35,8 @@ public class ProtocolFactory {
                 return new Protocol_v1_16_R2();
             case "v1_16_R3":
                 return new Protocol_v1_16_R3();
+            case "v1_16_R4":
+                return new Protocol_v1_16_R4();
             default:
                 throw new RuntimeException("No suitable protocal version found. Are you sure this version of minecraft is supported?");
         }
