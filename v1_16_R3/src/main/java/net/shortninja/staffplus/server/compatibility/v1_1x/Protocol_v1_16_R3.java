185c9aaa8b262e848a15d5d1c636f733b4ae26b2
DavidMaes <david.s.maes@gmail.com>
2022-06-03 21:33:58 +0200

    Fix vanish in spectator mode

--- v1_16_R3/src/main/java/net/shortninja/staffplus/server/compatibility/v1_1x/Protocol_v1_16_R3.java
+++ v1_16_R3/src/main/java/net/shortninja/staffplus/server/compatibility/v1_1x/Protocol_v1_16_R3.java
@@ -86,7 +86,7 @@ public class Protocol_v1_16_R3 implements IProtocol {
         } else
             packet = new PacketPlayOutPlayerInfo(EnumPlayerInfoAction.ADD_PLAYER, ((CraftPlayer) player).getHandle());
 
-        sendGlobalPacket(packet);
+        sendToAllButMe(packet, player);
     }
 
     @Override
@@ -99,10 +99,11 @@ public class Protocol_v1_16_R3 implements IProtocol {
         }
     }
 
-
-    private void sendGlobalPacket(Packet<?> packet) {
+    private void sendToAllButMe(Packet<?> packet, Player me) {
         for (Player player : Bukkit.getOnlinePlayers()) {
-            ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+            if(!me.getUniqueId().equals(player.getUniqueId())) {
+                ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+            }
         }
     }
 
