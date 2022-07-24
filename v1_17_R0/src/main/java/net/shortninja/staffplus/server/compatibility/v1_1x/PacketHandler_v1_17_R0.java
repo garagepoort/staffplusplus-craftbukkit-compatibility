d062269d5b38bb51372fa5d0907d1f85bf1b6e91
DavidMaes <david.s.maes@gmail.com>
2022-07-24 23:05:54 +0200

    Fix vanish sound

--- v1_17_R0/src/main/java/net/shortninja/staffplus/server/compatibility/v1_1x/PacketHandler_v1_17_R0.java
+++ v1_17_R0/src/main/java/net/shortninja/staffplus/server/compatibility/v1_1x/PacketHandler_v1_17_R0.java
@@ -3,6 +3,8 @@ package net.shortninja.staffplus.server.compatibility.v1_1x;
 import be.garagepoort.staffplusplus.craftbukkit.common.AbstractPacketHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
+import net.minecraft.network.protocol.game.ClientboundCustomSoundPacket;
+import net.minecraft.network.protocol.game.ClientboundSoundEntityPacket;
 import net.minecraft.network.protocol.game.ClientboundSoundPacket;
 import net.shortninja.staffplusplus.IStaffPlus;
 import org.bukkit.Bukkit;
@@ -17,10 +19,12 @@ public final class PacketHandler_v1_17_R0 extends AbstractPacketHandler {
 
     @Override
     public boolean onSend(ChannelHandlerContext context, Object o, ChannelPromise promise) throws Exception {
-        if (o instanceof ClientboundSoundPacket) {
+        if (o instanceof ClientboundSoundPacket ||
+            o instanceof ClientboundCustomSoundPacket ||
+            o instanceof ClientboundSoundEntityPacket) {
             RegisteredServiceProvider<IStaffPlus> provider = Bukkit.getServicesManager().getRegistration(IStaffPlus.class);
             if (provider != null) {
-                return !provider.getProvider().getSessionManager().get(player.getUniqueId()).isVanished();
+                return !provider.getProvider().getSessionManager().get(player).isVanished();
             }
         }
 
