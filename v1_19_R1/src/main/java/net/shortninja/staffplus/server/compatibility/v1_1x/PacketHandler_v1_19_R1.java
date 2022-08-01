5e6a0ef4ed5af7cf580e3091a82403d342140044
DavidMaes <david.s.maes@gmail.com>
2022-08-01 10:55:50 +0200

    update for 1.19.1

---
+++ v1_19_R1/src/main/java/net/shortninja/staffplus/server/compatibility/v1_1x/PacketHandler_v1_19_R1.java
@@ -0,0 +1,38 @@
+package net.shortninja.staffplus.server.compatibility.v1_1x;
+
+import be.garagepoort.staffplusplus.craftbukkit.common.AbstractPacketHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import net.minecraft.network.protocol.game.ClientboundCustomSoundPacket;
+import net.minecraft.network.protocol.game.ClientboundSoundEntityPacket;
+import net.minecraft.network.protocol.game.ClientboundSoundPacket;
+import net.shortninja.staffplusplus.IStaffPlus;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public final class PacketHandler_v1_19_R1 extends AbstractPacketHandler {
+
+    public PacketHandler_v1_19_R1(Player player) {
+        super(player);
+    }
+
+    @Override
+    public boolean onSend(ChannelHandlerContext context, Object o, ChannelPromise promise) {
+        if (o instanceof ClientboundSoundPacket ||
+            o instanceof ClientboundCustomSoundPacket ||
+            o instanceof ClientboundSoundEntityPacket) {
+            RegisteredServiceProvider<IStaffPlus> provider = Bukkit.getServicesManager().getRegistration(IStaffPlus.class);
+            if (provider != null) {
+                return !provider.getProvider().getSessionManager().get(player).isVanished();
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onReceive(ChannelHandlerContext context, Object o) throws Exception {
+        return true;
+    }
+}
