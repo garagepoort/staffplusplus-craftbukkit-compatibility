a94529ce56a99c33e755d03868bd85aa5f29852d
DavidMaes <david.s.maes@gmail.com>
2022-08-01 10:55:50 +0200

    update for 1.19.1

--- StaffPlusPlusCraftbukkitAPI/src/main/java/be/garagepoort/staffplusplus/craftbukkit/api/JsonSenderFactory.java
+++ StaffPlusPlusCraftbukkitAPI/src/main/java/be/garagepoort/staffplusplus/craftbukkit/api/JsonSenderFactory.java
@@ -1,38 +1,24 @@
 package be.garagepoort.staffplusplus.craftbukkit.api;
 
-import be.garagepoort.staffplusplus.craftbukkit.common.IProtocol;
 import be.garagepoort.staffplusplus.craftbukkit.common.json.rayzr.JsonSender;
-import net.shortninja.staffplus.server.compatibility.v1_12_R1.Protocol_v1_12_R1;
-import net.shortninja.staffplus.server.compatibility.v1_13_R1.Protocol_v1_13_R1;
-import net.shortninja.staffplus.server.compatibility.v1_13_R2.Protocol_v1_13_R2;
-import net.shortninja.staffplus.server.compatibility.v1_14_R1.Protocol_v1_14_R1;
-import net.shortninja.staffplus.server.compatibility.v1_14_R2.Protocol_v1_14_R2;
 import net.shortninja.staffplus.server.compatibility.v1_1x.JsonSender_v1_17_R0;
 import net.shortninja.staffplus.server.compatibility.v1_1x.JsonSender_v1_18_R0;
 import net.shortninja.staffplus.server.compatibility.v1_1x.JsonSender_v1_18_R1;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_15_R1;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_16_R1;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_16_R2;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_16_R3;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_17_R0;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_18_R0;
-import net.shortninja.staffplus.server.compatibility.v1_1x.Protocol_v1_18_R1;
 import org.bukkit.Bukkit;
 
 public class JsonSenderFactory {
 
     public static JsonSender getSender() {
-        final String version = Bukkit.getServer().getClass().getPackage().getName();
-        final String formattedVersion = version.substring(version.lastIndexOf('.') + 1);
-        switch (formattedVersion) {
-            case "v1_17_R1":
+        final String version = Bukkit.getBukkitVersion();
+        switch (version) {
+            case "1.17-R0.1-SNAPSHOT":
                 return new JsonSender_v1_17_R0();
-            case "v1_18_R1":
+            case "1.18-R0.1-SNAPSHOT":
                 return new JsonSender_v1_18_R0();
-            case "v1_18_R2":
+            case "1.18.2-R0.1-SNAPSHOT":
                 return new JsonSender_v1_18_R1();
             default:
-                throw new RuntimeException("No suitable jsonsender version found. Are you sure this version of minecraft is supported?");
+                throw new RuntimeException("No suitable protocol version found for: " + version + ". Are you sure this version of minecraft is supported?");
         }
     }
 }
--- StaffPlusPlusCraftbukkitAPI/src/main/java/be/garagepoort/staffplusplus/craftbukkit/api/ProtocolFactory.java
+++ StaffPlusPlusCraftbukkitAPI/src/main/java/be/garagepoort/staffplusplus/craftbukkit/api/ProtocolFactory.java
@@ -12,37 +12,36 @@ import org.bukkit.Bukkit;
 public class ProtocolFactory {
 
     public static IProtocol getProtocol() {
-        final String version = Bukkit.getServer().getClass().getPackage().getName();
-        final String formattedVersion = version.substring(version.lastIndexOf('.') + 1);
-        switch (formattedVersion) {
-            case "v1_12_R1":
+        final String version = Bukkit.getBukkitVersion();
+        switch (version) {
+            case "1.12.1-R0.1-SNAPSHOT":
                 return new Protocol_v1_12_R1();
-            case "v1_13_R1":
+            case "1.13.1-R0.1-SNAPSHOT":
                 return new Protocol_v1_13_R1();
-            case "v1_13_R2":
+            case "1.13.2-R0.1-SNAPSHOT":
                 return new Protocol_v1_13_R2();
-            case "v1_14_R1":
-            case "v1_14_R2":
+            case "1.14.1-R0.1-SNAPSHOT":
+            case "1.14.2-R0.1-SNAPSHOT":
                 return new Protocol_v1_14_R1();
-            case "v1_14_R3":
-            case "v1_14_R4":
+            case "1.14.3-R0.1-SNAPSHOT":
+            case "1.14.4-R0.1-SNAPSHOT":
                 return new Protocol_v1_14_R2();
-            case "v1_15_R1":
+            case "1.15.1-R0.1-SNAPSHOT":
                 return new Protocol_v1_15_R1();
-            case "v1_16_R1":
+            case "1.16.1-R0.1-SNAPSHOT":
                 return new Protocol_v1_16_R1();
-            case "v1_16_R2":
+            case "1.16.3-R0.1-SNAPSHOT":
                 return new Protocol_v1_16_R2();
-            case "v1_16_R3":
+            case "1.16.4-R0.1-SNAPSHOT":
                 return new Protocol_v1_16_R3();
-            case "v1_17_R1":
+            case "1.17-R0.1-SNAPSHOT":
                 return new Protocol_v1_17_R0();
-            case "v1_18_R1":
+            case "1.18-R0.1-SNAPSHOT":
                 return new Protocol_v1_18_R0();
-            case "v1_18_R2":
+            case "1.18.2-R0.1-SNAPSHOT":
                 return new Protocol_v1_18_R1();
             default:
-                throw new RuntimeException("No suitable protocal version found. Are you sure this version of minecraft is supported?");
+                throw new RuntimeException("No suitable protocol version found for: " + version + ". Are you sure this version of minecraft is supported?");
         }
     }
 }
