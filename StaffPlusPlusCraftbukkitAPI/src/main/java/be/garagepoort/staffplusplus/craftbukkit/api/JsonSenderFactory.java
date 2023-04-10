cbfe9f1bd14e3cf0745d02c0f4330819610d74bb
DavidMaes <david.s.maes@gmail.com>
2023-04-10 21:43:27 +0200

    Support version without snapshot

--- StaffPlusPlusCraftbukkitAPI/src/main/java/be/garagepoort/staffplusplus/craftbukkit/api/JsonSenderFactory.java
+++ StaffPlusPlusCraftbukkitAPI/src/main/java/be/garagepoort/staffplusplus/craftbukkit/api/JsonSenderFactory.java
@@ -14,21 +14,23 @@ public class JsonSenderFactory {
 
     public static JsonSender getSender() {
         final String version = Bukkit.getBukkitVersion();
-        switch (version) {
-            case "1.17-R0.1-SNAPSHOT":
+        final String versionWithoutSnapshot = version.replaceAll("-SNAPSHOT", "");
+
+        switch (versionWithoutSnapshot) {
+            case "1.17-R0.1":
                 return new JsonSender_v1_17_R0();
-            case "1.18-R0.1-SNAPSHOT":
+            case "1.18-R0.1":
                 return new JsonSender_v1_18_R0();
-            case "1.18.2-R0.1-SNAPSHOT":
+            case "1.18.2-R0.1":
                 return new JsonSender_v1_18_R1();
-            case "1.19-R0.1-SNAPSHOT":
+            case "1.19-R0.1":
                 return new JsonSender_v1_19_R0();
-            case "1.19.1-R0.1-SNAPSHOT":
-            case "1.19.2-R0.1-SNAPSHOT":
+            case "1.19.1-R0.1":
+            case "1.19.2-R0.1":
                 return new JsonSender_v1_19_R1();
-            case "1.19.3-R0.1-SNAPSHOT":
+            case "1.19.3-R0.1":
                 return new JsonSender_v1_19_R2();
-            case "1.19.4-R0.1-SNAPSHOT":
+            case "1.19.4-R0.1":
                 return new JsonSender_v1_19_R3();
             default:
                 throw new RuntimeException("No suitable protocol version found for: " + version + ". Are you sure this version of minecraft is supported?");
--- StaffPlusPlusCraftbukkitAPI/src/main/java/be/garagepoort/staffplusplus/craftbukkit/api/ProtocolFactory.java
+++ StaffPlusPlusCraftbukkitAPI/src/main/java/be/garagepoort/staffplusplus/craftbukkit/api/ProtocolFactory.java
@@ -13,43 +13,44 @@ public class ProtocolFactory {
 
     public static IProtocol getProtocol() {
         final String version = Bukkit.getBukkitVersion();
-        switch (version) {
-            case "1.12.1-R0.1-SNAPSHOT":
-            case "1.12.2-R0.1-SNAPSHOT":
+        final String versionWithoutSnapshot = version.replaceAll("-SNAPSHOT", "");
+        switch (versionWithoutSnapshot) {
+            case "1.12.1-R0.1":
+            case "1.12.2-R0.1":
                 return new Protocol_v1_12_R1();
-            case "1.13.1-R0.1-SNAPSHOT":
+            case "1.13.1-R0.1":
                 return new Protocol_v1_13_R1();
-            case "1.13.2-R0.1-SNAPSHOT":
+            case "1.13.2-R0.1":
                 return new Protocol_v1_13_R2();
-            case "1.14.1-R0.1-SNAPSHOT":
-            case "1.14.2-R0.1-SNAPSHOT":
+            case "1.14.1-R0.1":
+            case "1.14.2-R0.1":
                 return new Protocol_v1_14_R1();
-            case "1.14.3-R0.1-SNAPSHOT":
-            case "1.14.4-R0.1-SNAPSHOT":
+            case "1.14.3-R0.1":
+            case "1.14.4-R0.1":
                 return new Protocol_v1_14_R2();
-            case "1.15.1-R0.1-SNAPSHOT":
-            case "1.15.2-R0.1-SNAPSHOT":
+            case "1.15.1-R0.1":
+            case "1.15.2-R0.1":
                 return new Protocol_v1_15_R1();
-            case "1.16.1-R0.1-SNAPSHOT":
+            case "1.16.1-R0.1":
                 return new Protocol_v1_16_R1();
-            case "1.16.3-R0.1-SNAPSHOT":
+            case "1.16.3-R0.1":
                 return new Protocol_v1_16_R2();
-            case "1.16.4-R0.1-SNAPSHOT":
+            case "1.16.4-R0.1":
                 return new Protocol_v1_16_R3();
-            case "1.17-R0.1-SNAPSHOT":
+            case "1.17-R0.1":
                 return new Protocol_v1_17_R0();
-            case "1.18-R0.1-SNAPSHOT":
+            case "1.18-R0.1":
                 return new Protocol_v1_18_R0();
-            case "1.18.2-R0.1-SNAPSHOT":
+            case "1.18.2-R0.1":
                 return new Protocol_v1_18_R1();
-            case "1.19-R0.1-SNAPSHOT":
+            case "1.19-R0.1":
                 return new Protocol_v1_19_R0();
-            case "1.19.1-R0.1-SNAPSHOT":
-            case "1.19.2-R0.1-SNAPSHOT":
+            case "1.19.1-R0.1":
+            case "1.19.2-R0.1":
                 return new Protocol_v1_19_R1();
-            case "1.19.3-R0.1-SNAPSHOT":
+            case "1.19.3-R0.1":
                 return new Protocol_v1_19_R2();
-            case "1.19.4-R0.1-SNAPSHOT":
+            case "1.19.4-R0.1":
                 return new Protocol_v1_19_R3();
             default:
                 throw new RuntimeException("No suitable protocol version found for: " + version + ". Are you sure this version of minecraft is supported?");
