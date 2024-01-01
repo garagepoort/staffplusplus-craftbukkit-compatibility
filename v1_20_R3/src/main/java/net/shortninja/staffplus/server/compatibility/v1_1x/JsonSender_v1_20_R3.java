36a64c771d2c102151aff12534eae0889888e46a
Adamiok <adam@kresta.eu>
2024-01-01 10:46:20 +0400

    Add 1_20_R3 protocol

---
+++ v1_20_R3/src/main/java/net/shortninja/staffplus/server/compatibility/v1_1x/JsonSender_v1_20_R3.java
@@ -0,0 +1,55 @@
+package net.shortninja.staffplus.server.compatibility.v1_1x;
+
+import be.garagepoort.staffplusplus.craftbukkit.common.json.rayzr.JSONMessage;
+import be.garagepoort.staffplusplus.craftbukkit.common.json.rayzr.JsonSender;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.bukkit.entity.Player;
+
+import org.bukkit.craftbukkit.v1_20_R3.entity.CraftPlayer;
+
+import java.lang.reflect.Method;
+
+public class JsonSender_v1_20_R3 implements JsonSender {
+
+    @Override
+    public void send(JSONMessage jsonMessage, Player...players) {
+        sendPacket(createTextPacket(jsonMessage.toString()), players);
+    }
+
+    private void sendPacket(ClientboundSystemChatPacket packet, Player... players) {
+        if (packet == null) {
+            return;
+        }
+        
+        // The 2 `send` methods are called different in obfuscated code, so we have to do this workaround to get the correct one
+        Method sendMethod;
+        try {
+            Class[] args = new Class[1];
+            args[0] = Packet.class;
+            sendMethod = ServerGamePacketListenerImpl.class.getMethod("b", args);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("Failed to get `send` method on connection", e);
+        }
+        
+        for (Player player : players) {
+            ServerGamePacketListenerImpl serverGamePacketListener = ((CraftPlayer) player).getHandle().connection;
+            
+            try {
+                sendMethod.invoke(serverGamePacketListener, packet);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to send packet", e);
+            }
+        }
+    }
+
+    private ClientboundSystemChatPacket createTextPacket(String message) {
+        try {
+            return new ClientboundSystemChatPacket(Component.Serializer.fromJson(message), false);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to serialize json packet message. Is it valid JSON?", e);
+        }
+    }
+}
